8e19323d539c5f05100817f8e87bbe8b5b2e3826
bakdominik <dominikbak93@gmail.com>
2020-09-06 11:47:40 +0200

    edit projects view

--- base/models.py
+++ base/models.py
@@ -10,7 +10,7 @@ class Tag(models.Model):
 class Project(models.Model):
     headline = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    thumbnail = models.ImageField(null=True, blank=True, upload_to="static", default="images/placeholder.png")
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="images/placeholder.png")
     tags = models.ManyToManyField(Tag, null=True, blank=True)
     live = models.CharField(max_length=200)
     github = models.CharField(max_length=200)
