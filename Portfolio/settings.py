1e6b1e7979eaa80dd4166cbeddcc2dee74d626e6
bakdominik <dominikbak93@gmail.com>
2020-09-07 11:10:40 +0200

    edit urls

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -134,10 +134,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 mimetypes.add_type("text/css", ".css", True)
 
